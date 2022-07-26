0654c171efec84721bc0e8feac1c070e6295d4c9
emilianobono <emilianobono88@gmail.com>
2022-07-26 09:23:29 -0300

    primer entrega

--- script.js
+++ script.js
@@ -1,24 +1,103 @@
-let summit = parseInt(prompt("Enter the maximum number"));
-while (!summit) {
-    summit = parseInt(prompt("Enter a correct number"));
+alert("Welcome to the Mortal Kombat Tournament")
+alert("The fighters are: Scorpio - Kitana - Kano - Milena")
+
+let jugador = prompt ("Select your fighter:").toLowerCase()
+
+// let jugador2 = prompt ("Ingresa el nombre del segundo jugador")
+
+while ((jugador != 'scorpio') && (jugador != 'kano') && (jugador != 'milena') && (jugador != 'kitana')){
+    jugador = prompt ("Select a valid fighter:").toLowerCase();
 }
 
-const targetNum = Math.floor(Math.random() * summit) + 1;
 
-let guess = parseInt(prompt("First guess"));
-let attempts = 1;
+let scorpio = 1000;
+let kano = 900;
+let kitana = 850;
+let milena = 850;
 
-while (parseInt(guess) !== targetNum) {
-    if (guess === 'q') break;
-    attempts++;
-    if (guess > targetNum) {
-        guess = (prompt("Exceeds! Try again:"));
-    } else {
-        guess = (prompt("Too low! Try again:"));
+let sindel = 400;
+let subzero = 450;
+let reptile = 500;
+
+let lifepoints = 0
+
+alert("You procede to the first encounter: Sindel")
+
+function encounter1() {
+    switch (jugador) {
+        case 'scorpio':
+            lifepoints = (scorpio - sindel)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        case 'kano':
+            lifepoints = (kano - sindel)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        case 'kitana':
+            lifepoints = (kitana - sindel)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        case 'milena':
+            lifepoints = (milena - sindel)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        default:
+            alert("Not a valid fighter, start again.")
+            break;
     }
 }
-if (guess === 'q') {
-    alert("Good bye!")
-} else {
-    alert(`You are a winner! It took you ${attempts} guesses`)
+encounter1()
+
+alert("Victory! Now face the next fighter: Sub-Zero")
+
+function encounter2() {
+    switch (jugador) {
+        case 'scorpio':
+            lifepoints = (lifepoints - subzero)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        case 'kano':
+            lifepoints = (lifepoints - subzero)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        case 'kitana':
+            lifepoints = (lifepoints - subzero)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        case 'milena':
+            lifepoints = (lifepoints - subzero)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        default:
+            alert("Not a valid fighter, start again.")
+            break;
+    }
 }
+encounter2()
+
+if (lifepoints <= 0) {
+    alert("Game over! You lose!")
+} else if (lifepoints >= 0){
+    alert("Victory! Now face the next fighter: Reptile")
+}
+
+function encounter3() {
+    switch (jugador) {
+        case 'scorpio':
+            lifepoints = (lifepoints - reptile)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        case 'kano':
+            lifepoints = (lifepoints - reptile)
+            alert("Your fighter's lifepoints are " + lifepoints)
+            break;
+        default:
+            alert("Not a valid fighter, start again.")
+            break;
+    }
+}
+encounter3()
+
+if (lifepoints <= 0) {
+    alert("Game over! You lose!")
+} 
