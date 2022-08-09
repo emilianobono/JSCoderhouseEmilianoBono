81522b25ae07fcf1a3b1724c67216cf3655ddc03
emilianobono <emilianobono88@gmail.com>
2022-08-09 20:23:55 -0300

    ecommerce

--- script.js
+++ script.js
@@ -1,103 +1,115 @@
-alert("Welcome to the Mortal Kombat Tournament")
-alert("The fighters are: Scorpio - Kitana - Kano - Milena")
+const productList = [
+    {
+        name: 'Kitty Flip-flops',
+        number: '1',
+        price: 10,
+        color: 'purple',
+        stock: 3,
+        brand: 'Kitty Girl'
+    },
+    {
+        name: 'Cat Hat',
+        number: '2',
+        price: 5,
+        color: 'black',
+        stock: 1,
+        brand: 'Catwoman'
+    },
+    {
+        name: 'Kitty Pijamas',
+        number: '3',
+        price: 13,
+        color: 'pink',
+        stock: 3,
+        brand: 'Kitty Girl'
+    },
+    {
+        name: 'Kitty Sneakers',
+        number: '4',
+        price: 16,
+        color: 'white',
+        stock: 3,
+        brand: 'Kitty Girl'
+    },
+    {
+        name: 'Kitty Backpack',
+        number: '5',
+        price: 16,
+        color: 'purple',
+        stock: 1,
+        brand: 'Catwoman'
+    },
+    {
+        name: 'Kitty Suitcase',
+        number: '6',
+        price: 20,
+        color: 'black',
+        stock: 1,
+        brand: 'Catwoman'
+    }
+]
 
-let jugador = prompt ("Select your fighter:").toLowerCase()
+class Product {
+    constructor(name, number, price, color, stock, brand){
+        this.name = name;
+        this.number = number;
+        this.price = price;
+        this.color = color;
+        this.stock = stock;
+        this.brand = brand;
+    }
 
-// let jugador2 = prompt ("Ingresa el nombre del segundo jugador")
-
-while ((jugador != 'scorpio') && (jugador != 'kano') && (jugador != 'milena') && (jugador != 'kitana')){
-    jugador = prompt ("Select a valid fighter:").toLowerCase();
-}
-
-
-let scorpio = 1000;
-let kano = 900;
-let kitana = 850;
-let milena = 850;
-
-let sindel = 400;
-let subzero = 450;
-let reptile = 500;
-
-let lifepoints = 0
-
-alert("You procede to the first encounter: Sindel")
-
-function encounter1() {
-    switch (jugador) {
-        case 'scorpio':
-            lifepoints = (scorpio - sindel)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        case 'kano':
-            lifepoints = (kano - sindel)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        case 'kitana':
-            lifepoints = (kitana - sindel)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        case 'milena':
-            lifepoints = (milena - sindel)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        default:
-            alert("Not a valid fighter, start again.")
-            break;
+    dataInput(){
+        return `N°: ${this.number} > ${this.name} $${this.price}`;
+    }
+    notification(){
+        return `${this.name} the price is ${this.price}`
     }
 }
-encounter1()
 
-alert("Victory! Now face the next fighter: Sub-Zero")
+const shoppingKart = []
 
-function encounter2() {
-    switch (jugador) {
-        case 'scorpio':
-            lifepoints = (lifepoints - subzero)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        case 'kano':
-            lifepoints = (lifepoints - subzero)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        case 'kitana':
-            lifepoints = (lifepoints - subzero)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        case 'milena':
-            lifepoints = (lifepoints - subzero)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        default:
-            alert("Not a valid fighter, start again.")
-            break;
-    }
-}
-encounter2()
-
-if (lifepoints <= 0) {
-    alert("Game over! You lose!")
-} else if (lifepoints >= 0){
-    alert("Victory! Now face the next fighter: Reptile")
+// Funciones
+const productDataInput = (arrayProducts) => {
+    return arrayProducts.map( (element)=> element.dataInput()).join('\n')
 }
 
-function encounter3() {
-    switch (jugador) {
-        case 'scorpio':
-            lifepoints = (lifepoints - reptile)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        case 'kano':
-            lifepoints = (lifepoints - reptile)
-            alert("Your fighter's lifepoints are " + lifepoints)
-            break;
-        default:
-            alert("Not a valid fighter, start again.")
-            break;
-    }
+const addToShoppingKartByNumber = (products) => {
+    const productData = productDataInput(products);
+    const number = 1
+    do {
+    const number = prompt('Select the product number you want to buy:\n' + productData + '\n' + 'To exit, enter 0 or press Enter.' );
+    const product = products.find((product) => product.number === number);
+    if (!product) return;
+    shoppingKart.push(product);
+    alert('You have added the product to the Shopping Kart!')
+    } while (number !== '0');
 }
-encounter3()
 
-if (lifepoints <= 0) {
-    alert("Game over! You lose!")
-} 
+const showShoppingKart = (shoppingKartProducts) => {
+    shoppingKartProducts.forEach((product) => {
+        console.log(product.notification());
+    });
+}
+const getTotal = (arrayProducts) => {
+    let total = 0;
+    arrayProducts.forEach((product) => {
+        total += product.price;
+    });
+    return total;
+}
+
+const products = productList.map(product => new Product(
+    product.name,
+    product.number,
+    product.price,
+    product.color,
+    product.stock,
+    product.brand
+));
+
+addToShoppingKartByNumber(products)
+
+alert('Open the console to see the total amount to pay' + getTotal(shoppingKart))
+showShoppingKart(shoppingKart);
+console.log(getTotal(shoppingKart));
