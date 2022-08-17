f82299ef1874c92de136c77749b8e0d123fe119a
emilianobono <emilianobono88@gmail.com>
2022-08-16 21:20:34 -0300

    adding Events

--- script.js
+++ script.js
@@ -76,14 +76,11 @@ const productDataInput = (arrayProducts) => {
 
 const addToShoppingKartByNumber = (products) => {
     const productData = productDataInput(products);
-    const number = 1
-    do {
     const number = prompt('Select the product number you want to buy:\n' + productData + '\n' + 'To exit, enter 0 or press Enter.' );
     const product = products.find((product) => product.number === number);
     if (!product) return;
     shoppingKart.push(product);
     alert('You have added the product to the Shopping Kart!')
-    } while (number !== '0');
 }
 
 const showShoppingKart = (shoppingKartProducts) => {
@@ -108,8 +105,57 @@ const products = productList.map(product => new Product(
     product.brand
 ));
 
-addToShoppingKartByNumber(products)
+// addToShoppingKartByNumber(products)
 
-alert('Open the console to see the total amount to pay' + getTotal(shoppingKart))
-showShoppingKart(shoppingKart);
-console.log(getTotal(shoppingKart));
+
+// alert('You have to pay $' + getTotal(shoppingKart))
+// showShoppingKart(shoppingKart);
+// console.log(getTotal(shoppingKart));
+
+// let adding = document.getElementsByClassName("btn");
+// adding.addEventListener("click", addItem)
+// function addItem() {
+//     console.log("You have added the item to the shopping kart")
+// }
+
+const addToKart = document.getElementById("b1");
+addToKart.addEventListener("click", addToShoppingKartByNumber)
+function addToShoppingKartByNumber(products){
+    console.log("You have added the item to the shopping kart")
+}
+
+class User{
+    constructor(username, email, password) {
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+}
+const users = []
+
+const totalButton = document.getElementById("totalButton")
+const totalDiv = document.getElementById("totalDiv")
+
+totalButton.addEventListener("click", () =>{
+    divUsers.innerHTML += `
+        <div class="totalP" id="user${indice}" style="width: 18rem;margin:3px;">
+            <p>${getTotal(shoppingKart)}</p>
+        </div>
+    `
+})
+
+const formId = document.getElementById("formId")
+
+formId.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const username = document.getElementById("username").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+
+    const user = new User(username, email, password)
+    users.push(user)
+
+    formId.reset()
+
+    console.log(users)
+})
